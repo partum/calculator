d4e626143f8bb014acbcc453496f62c0d8c7a669
partum <jarringt@uccs.edu>
2021-02-26 11:01:56 -0700

    added buttons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const buttons = [
+  { id: "0", name: "0" },
+  { id: "1", name: "1" },
+  { id: "2", name: "2" },
+  { id: "3", name: "3" },
+  { id: "4", name: "4" },
+  { id: "5", name: "5" },
+  { id: "6", name: "6" },
+  { id: "7", name: "7" },
+  { id: "8", name: "8" },
+  { id: "9", name: "9" },
+];
+function App({ buttons }) {
+  const [number, setNumber] = useState("");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <>
+      <div>
+        <h1>{number}</h1>
+        {buttons.map((button) => (
+          <button key={button.id} onClick={() => setNumber(button.name)}>
+            {button.name}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+}
+
+ReactDOM.render(<App buttons={buttons} />, document.getElementById("root"));
